99efd7e79ccec5461d98509a4e2454d25e766f55
ShaiqRaza <shaiqraza808y@gmail.com>
2025-09-11 20:34:49 +0500

    update bytestream.cc functions

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,50 +6,62 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  uint64_t available = capacity_ - buffer_.size();
+  if(available >= data.size()) {
+    buffer_ += data;
+    bytes_pushed_ += data.size();
+  }//when data is bigger than the available space: to hum available space ko hi fill karein gai, baaqi data loss 
+  else {
+    buffer_ += data.substr(0, available);
+    bytes_pushed_ += available;
+  }
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return closed_; 
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return buffer_;
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  while(len > 0) {
+    buffer_.erase(0, 1)
+    bytes_popped_++;
+    len--;
+  }
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return buffer_.size() == 0 && closed_; // Your code here.
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size(); // Your code here.
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_; // Your code here.
 }
