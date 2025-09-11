7ffe11f77d3836d982271e05e57ca7e9882950f5
ShaiqRaza <shaiqraza808y@gmail.com>
2025-09-11 15:27:48 +0500

    second test passed

--- apps/webget.cc
+++ apps/webget.cc
@@ -10,13 +10,14 @@ using namespace std;
 void get_URL( const string& host, const string& path )
 {
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
   TCPSocket connectionSocket = TCPSocket();
   connectionSocket.connect( { host, "http" } );
   connectionSocket.write( "GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n" );
-  string response;
-  connectionSocket.read( response );
-  cerr << response << endl;
+  while (!connectionSocket.eof()) {
+    string buffer;
+    connectionSocket.read(buffer);
+    cout << buffer;
+}
 }
 
 int main( int argc, char* argv[] )
